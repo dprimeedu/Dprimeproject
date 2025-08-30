5a12efc50d5a8334977384561f002f3e3d3f9eb9
StraySpeed <yo2189@naver.com>
2025-08-30 12:01:59 +0900

    [Feat] underline custom filter

--- common/templatetags/common_filters.py
+++ common/templatetags/common_filters.py
@@ -9,3 +9,19 @@ def line_feed(value):
     """
     if not value: return
     return value.replace("\\r\\n", "<br>")
+
+
+@register.filter
+def underline(value):
+    """
+    밑줄 치는 커스텀 태그
+
+    "￰"(U+FFF0) 사용
+    """
+    if not value: return
+
+    import re
+    pattern = r"\uFFF0(.*?)\uFFF0"
+
+    text = re.sub(pattern, r"<ins>\1</ins>", value)
+    return text
